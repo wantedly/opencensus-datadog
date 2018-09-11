d34014184e8112f2194887365f1ce16dcc28fe90
Yuichi Saito <munisystem@gmail.com>
2018-09-11 18:52:06 +0900

    Fix NoMethodError

--- lib/opencensus/trace/exporters/datadog.rb
+++ lib/opencensus/trace/exporters/datadog.rb
@@ -13,8 +13,8 @@ module OpenCensus
 
         def self.log
           unless defined? @logger
-            @logger = Logger.new(STDOUT)
-            @logger.level = Logger::WARN
+            @logger = ::Logger.new(STDOUT)
+            @logger.level = ::Logger::WARN
           end
           @logger
         end
