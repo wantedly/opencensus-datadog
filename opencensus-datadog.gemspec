810f2adc0ef2dd359164cd7b7ecfe722e37a9e8d
yusuke ota <33338085+yusuke-ota@users.noreply.github.com>
2026-03-26 17:20:47 +0900

    Migrate ddtrace(v1) to datadog(v2). (#15)
    * change ruby support version.
    
    * change ddtrace(v1) to datadog(v2).
    
    * check DataDog constant values were not deleted.
    
    * revert OpenCensus::Datadog::VERSION change

--- opencensus-datadog.gemspec
+++ opencensus-datadog.gemspec
@@ -21,11 +21,13 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.required_ruby_version = ">= 3.2.0"
+
   spec.add_dependency 'msgpack'
   spec.add_dependency 'opencensus'
-  spec.add_dependency 'ddtrace', '~> 1.0'
+  spec.add_dependency 'datadog', '>= 2.0', '< 3.0'
 
   spec.add_development_dependency "bundler", ">= 1.16", "< 3.0"
-  spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "rake", "~> 13.0"
   spec.add_development_dependency "rspec", "~> 3.0"
 end
--- spec/opencensus/trace/exporters/datadog_spec.rb
+++ spec/opencensus/trace/exporters/datadog_spec.rb
@@ -6,4 +6,23 @@ describe OpenCensus::Trace::Exporters::Datadog do
       expect(OpenCensus::Trace::Exporters::Datadog.log.level).to eq ::Logger::WARN
     end
   end
+
+  describe 'Datadog gem constants compatibility' do
+    let(:converter) { OpenCensus::Trace::Exporters::Datadog::Converter.new('test-service') }
+
+    it 'has access to TAG_SAMPLING_PRIORITY constant' do
+      expect(::Datadog::Tracing::Metadata::Ext::Distributed::TAG_SAMPLING_PRIORITY).to be_a(String)
+      expect(OpenCensus::Trace::Exporters::Datadog::Converter::DATADOG_SAMPLING_PRIORITY_KEY).to eq(
+        ::Datadog::Tracing::Metadata::Ext::Distributed::TAG_SAMPLING_PRIORITY
+      )
+    end
+
+    it 'has access to Priority::AUTO_KEEP constant' do
+      expect(::Datadog::Tracing::Sampling::Ext::Priority::AUTO_KEEP).to be_a(Integer)
+    end
+
+    it 'has access to Errors::TAG_MSG constant' do
+      expect(::Datadog::Tracing::Metadata::Ext::Errors::TAG_MSG).to be_a(String)
+    end
+  end
 end
